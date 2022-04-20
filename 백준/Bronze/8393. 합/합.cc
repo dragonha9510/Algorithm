ce28e89339ac52b52a2a37536539b62a3a6d35bc
dragonha9510 <70867534+dragonha9510@users.noreply.github.com>
2022-04-20 23:36:11 +0900

    [Bronze III] Title: 합, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int iNum = 0;		// 입력 받을 변수
+
+	cin >> iNum;
+
+	cout << iNum * (iNum + 1) / 2;
+    
+    /*
+    int iRes = 0;
+    
+    for(int i = 0; i <= iNum; ++i)
+    {
+        iRes += i;    
+    }
+    
+    cout << iRes;
+    */
+}
