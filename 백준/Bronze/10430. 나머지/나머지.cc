9f6df38dae09b5d8f5546d17cb78dae8f9e55f8a
dragonha9510 <70867534+dragonha9510@users.noreply.github.com>
2022-05-05 22:10:41 +0900

    [Bronze V] Title: 나머지, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.cc
@@ -0,0 +1,22 @@
+//첫째 줄에 (A+B)%C, 
+//둘째 줄에 ((A%C) + (B%C))%C, 
+//셋째 줄에 (A×B)%C, 
+//넷째 줄에 ((A%C) × (B%C))%C를 출력한다.
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int A = 0, B = 0, C = 0;
+    
+    cin >> A >> B >> C;
+    
+    cout << (A + B) % C << endl;
+    cout << ((A % C) + (B % C)) % C << endl;
+    cout << (A * B) % C << endl;
+    cout << ((A % C) * (B % C)) % C << endl;
+    
+    return 0;
+}
