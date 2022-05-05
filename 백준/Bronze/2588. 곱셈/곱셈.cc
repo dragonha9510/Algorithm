8b7f10ee3412e88433a43f120cb8052504097d17
dragonha9510 <70867534+dragonha9510@users.noreply.github.com>
2022-05-05 22:16:11 +0900

    [Bronze IV] Title: 곱셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int A = 0;
+    int B = 0;
+    
+    cin >> A >> B;
+    
+    cout << A * (B % 10) << endl;
+    cout << A * (((B % 100) - (B % 10)) / 10) << endl;
+    cout << A * (B / 100) << endl;
+    cout << A * B << endl;
+    
+    return 0;
+}
