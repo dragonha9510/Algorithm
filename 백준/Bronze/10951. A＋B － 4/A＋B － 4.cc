70f8852a03117243e0c0d23f67f77bddd9feabfd
dragonha9510 <70867534+dragonha9510@users.noreply.github.com>
2022-05-06 14:02:04 +0900

    [Bronze III] Title: A+B - 4, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int iA = 0, iB = 0;
+    
+    while(cin >> iA >> iB)
+    {
+        cout << iA + iB << endl;
+    }
+    
+    return 0;
+}
