9d01d831ab0bdc4693ba681878b42fa2334c75a2
dragonha9510 <70867534+dragonha9510@users.noreply.github.com>
2022-05-12 17:45:28 +0900

    [Bronze II] Title: 숫자의 개수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int iArr[10] = {0};
+
+	int hundMill = 100000000;
+
+	int iPro = 0;
+	int iA, iB, iC;
+
+	cin >> iA >> iB >> iC;
+
+	iPro = iA * iB * iC;
+
+	if (!(iPro / hundMill))
+		hundMill /= 10;
+
+	if (!(iPro / hundMill))
+		hundMill /= 10;
+
+	for (int i = 0; i < 9; ++i)
+	{
+		iArr[iPro / hundMill]++;
+
+		iPro %= hundMill;
+
+		hundMill /= 10;
+
+		if (hundMill == 0)
+			break;
+	}
+
+	for (int i = 0; i < 10; ++i)
+		cout << iArr[i] << endl;
+
+	return 0;
+}
