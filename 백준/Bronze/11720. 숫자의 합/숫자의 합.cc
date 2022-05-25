9b38dcc93ab0861dace425133aea17390c936966
dragonha9510 <70867534+dragonha9510@users.noreply.github.com>
2022-05-25 23:48:51 +0900

    [Bronze II] Title: 숫자의 합, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+#define CHARTOINT(t) (int)t - 48
+
+int main()
+{
+    int iSize = 0;
+    char strInput[101];
+    int iRes = 0;
+    
+    cin >> iSize;
+    cin >> strInput;
+    
+    for(size_t i = 0; i < iSize; ++i)
+    {
+        iRes += CHARTOINT(strInput[i]);
+    }
+    
+    cout << iRes;
+    
+    return 0;
+}
